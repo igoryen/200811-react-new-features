c49ba202554d787ee71037c3d725b2bf1d1f4498
igoryen <igor.yentaltsev@gmail.com>
2020-08-11 15:34:04 -0400

    189. Using Create React App.

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>my new content</div>,
   document.getElementById('root')
 );
 
