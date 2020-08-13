6ae1e1b9595f26e13e4c76fac746c4bd9fa7f7f4
igoryen <igor.yentaltsev@gmail.com>
2020-08-13 16:17:25 -0400

    197B. The Context API & useContext Hook: Part I.

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -1,22 +1,13 @@
 import React, { useState, useEffect, useReducer } from 'react'
 import notesReducer from '../reducers/notes'
-import Note from './Note'
+import NoteList from './NoteList'
+import AddNoteForm from './AddNoteForm'
 
 const NoteApp = () => {
     const [notes, dispatch] = useReducer(notesReducer, [])
-    const [title, setTitle] = useState('')
-    const [body, setBody] = useState('')
 
-    const addNote = (e) => {
-        e.preventDefault()
-        dispatch({
-            type: 'ADD_NOTE',
-            title,
-            body
-        })
-        setTitle('')
-        setBody('')
-    }
+
+
 
     const removeNote = (title) => {
         dispatch({
@@ -42,16 +33,8 @@ const NoteApp = () => {
     return (
         <div>
             <h1>Notes</h1>
-            {notes.map( (note) => (
-                <Note key={note.title} note={note} removeNote={removeNote} />
-            ))}
-            <p>Add note</p>
-
-            <form onSubmit={addNote}>
-                <input value={title} onChange={(e) => setTitle(e.target.value)} />
-                <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
-                <button>add note</button>
-            </form>
+            <NoteList notes={notes} removeNote={removeNote} />
+            <AddNoteForm dispatch={dispatch} />
         </div>
     )
 }
